ebf040cdc80301363bfd0c115c5a0fa2ab01d6be
Sunday MASEKO <sumaseko@c5r10s11.wethinkcode.co.za>
2019-06-04 17:31:30 +0200

    libft.h

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumaseko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/04 17:01:44 by sumaseko          #+#    #+#             */
+/*   Updated: 2019/06/04 17:19:19 by sumaseko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIBFT_H
+# define FT_LIBFT_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <type.h>
+
+
