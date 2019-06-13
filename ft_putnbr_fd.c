b4b092137a7e24d5d2dfef4a0f39ffc12afe9836
Sunday MASEKO <sumaseko@c4r9s8.wethinkcode.co.za>
2019-06-13 10:58:30 +0200

    updated

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,31 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sumaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/12 14:13:05 by sumaseko          #+#    #+#             */
-/*   Updated: 2019/06/13 10:37:14 by sumaseko         ###   ########.fr       */
+/*   Created: 2019/06/13 10:37:25 by sumaseko          #+#    #+#             */
+/*   Updated: 2019/06/13 10:56:11 by sumaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr(int n)
+void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
-		ft_putstr("-2147483648");
+		ft_putstr_fd("-2147483648", fd);
 	else if (n < 0)
 	{
-		ft_putchar('-');
-		ft_putnbr(-n);
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
 	}
 	else if (n >= 10)
 	{
-		ft_putnbr(n / 10);
-		ft_putchar(n % 10 + '0');
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd(n % 10 + '0', fd);
 	}
 	else
-	ft_putchar(n + '0');
+	ft_putchar_fd(n + '0', fd);
 }
